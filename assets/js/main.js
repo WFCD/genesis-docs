f04808567ff7a27c49e3d11cfc7ec2d58d63dc8e
Tobiah <tobiah@protonmail.com>
2018-01-31 15:59:55 +0000

    fix theme not being set

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 /* globals $,  Cookies */
 
-// Nothing, yet
+const setTheme = (themeName, superTheme, reset) => {
+  const theme = $('#mode');
+  const themecustom = $('#mode-custom');
+  if (!reset) {
+    if (theme.hasClass(themeName)) {
+      setTheme(false, false, true);
+    } else {
+      theme.attr('href', `css/bootstrap-${superTheme}.min.css`);
+      themecustom.attr('href', `css/main.${themeName}.css`);
+      theme.removeClass();
+      theme.addClass(themeName);
+      Cookies.set('mode', themeName);
+    }
+  } else {
+    theme.attr('href', 'css/bootstrap-default.min.css');
+    themecustom.attr('href', 'css/main.css');
+    theme.removeClass();
+    Cookies.set('mode', 'day');
+  }
+};
