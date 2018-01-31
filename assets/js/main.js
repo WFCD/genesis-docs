c3f5b05e521ffecc4be3eca29da12bf945a54496
Tobiah <tobiah@protonmail.com>
2018-01-31 19:09:58 +0000

    ignore modal links for smooth scrolling

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,8 @@ $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
+  .not('[href="#license"]') //ignore modal links
+  .not('[href="#credits"]')
   .click(function(event) {
     // On-page links
     if (
@@ -50,7 +52,7 @@ $('a[href*="#"]')
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            // $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
           }
         });
