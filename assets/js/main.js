885e5a3524e4fbb61898fcb195224f559553c507
Tobiah <tobiah@protonmail.com>
2018-04-12 21:09:56 -0500

    revert bootstrap 4 changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,27 +6,6 @@ $body.scrollspy({
     offset: 205
 })
 
-const setTheme = (themeName, superTheme, reset) => {
-  const theme = $('#mode');
-  const themecustom = $('#mode-custom');
-  if (!reset) {
-    if (theme.hasClass(themeName)) {
-      setTheme(false, false, true);
-    } else {
-      theme.attr('href', `/css/bootstrap-${superTheme}.min.css`);
-      themecustom.attr('href', `/css/main.${themeName}.css`);
-      theme.removeClass();
-      theme.addClass(themeName);
-      Cookies.set('mode', themeName);
-    }
-  } else {
-    theme.attr('href', '/css/bootstrap-default.min.css');
-    themecustom.attr('href', '/css/main.css');
-    theme.removeClass();
-    Cookies.set('mode', 'day');
-  }
-};
-
 /* Based on the solution here: https://css-tricks.com/snippets/jquery/smooth-scrolling/  */
 // Select all links with hashes
 $('a[href*="#"]')
