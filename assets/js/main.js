d75bf1074e08d75c01036515edcd495aa3299faa
Tobiah <tobiah@protonmail.com>
2018-04-13 00:27:58 -0500

    basic guild view
     no actual setting changing
     dump of channel ids in a dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
-/* globals $,  Cookies, location */
+'use strict';
+
+/* globals $, document, window */
 
 const $body = $(document.body);
 $body.scrollspy({
-    target: '#affix-nav',
-    offset: 205
-})
+  target: '#affix-nav',
+  offset: 205,
+});
 
 /* Based on the solution here: https://css-tricks.com/snippets/jquery/smooth-scrolling/  */
 // Select all links with hashes
@@ -12,34 +14,34 @@ $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .not('[href="#license"]') //ignore modal links
+  .not('[href="#license"]') // ignore modal links
   .not('[href="#credits"]')
-  .click(function(event) {
+  .click((event) => {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      window.location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')
       &&
-      location.hostname == this.hostname
+      window.location.hostname === this.hostname
     ) {
       // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      let target = $(this.hash);
+      target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
       // Does a scroll target exist?
       if (target.length) {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
+          scrollTop: target.offset().top,
+        }, 1000, () => {
           // Callback after animation
           // Must change focus!
-          var $target = $(target);
+          const $target = $(target);
           $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
+          if ($target.is(':focus')) { // Checking if the target was focused
             return false;
-          } else {
-            $target.focus(); // Set focus again
           }
+          $target.focus(); // Set focus again
+          return true;
         });
       }
     }
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Routing page for pages on the root level
 
 const express = require('express');
@@ -11,6 +13,23 @@ const router = express.Router();
 
 logger.level = process.env.LOG_LEVEL || 'error'; // default to error, we don't need everything
 
+const defaults = {
+  prefix: '/',
+  respond_to_settings: true,
+  platform: 'pc',
+  language: 'en',
+  delete_after_respond: true,
+  delete_response: true,
+  createPrivateChannel: false,
+  deleteExpired: false,
+  allowCustom: false,
+  allowInline: false,
+  defaultRoomsLocked: true,
+  defaultNoText: false,
+  defaultShown: false,
+  tempCategory: false,
+};
+
 const opts = {
   supportBigNumbers: true,
   bigNumberStrings: true,
@@ -19,8 +38,132 @@ const opts = {
   user: process.env.MYSQL_USER || 'genesis',
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB || 'genesis',
+  Promise,
 };
 const db = mysql.createPool(opts);
+
+/* Utility Functions */
+/**
+ * Doing this as we need to get the guild list each time they go to a page.
+ *
+ * @param  {Object} req request to get information from
+ * @returns {Array.<Object>} list of accessible guilds
+ */
+async function getGuilds(req) {
+  let guilds;
+  if (req.isAuthenticated()) {
+    ({ guilds } = req.user);
+    const guildIds = guilds ? guilds.map(guild => guild.id) : [];
+
+    const query = SQL`select distinct guild_id from channels where guild_id in (${guildIds});`;
+    const queryResult = (await db.query(query))[0].map(row => row.guild_id);
+
+    guilds = guilds.filter((guild) => {
+      const hasGuild = queryResult.indexOf(guild.id) !== -1;
+
+      const perm = new Discord.Permissions(guild.permissions);
+      const hasPerm = perm.has('MANAGE_ROLES', true);
+      return hasPerm && hasGuild;
+    });
+  }
+  return guilds;
+}
+
+/**
+ * Aggregate channel settings
+ * @param  {Object}        guild Guild to get channel settings for
+ * @returns {Object.<Object>}         settings for each channel in the array
+ */
+async function aggregateChannels(guild) {
+  const channelIds = (await db.query(SQL`select distinct id from channels where guild_id = ${guild.id};`))[0].map(row => row.id);
+  const settings = {};
+  channelIds.forEach((channelId) => {
+    settings[channelId] = {};
+  });
+  const channelQuery = SQL`select * from settings where channel_id in (${channelIds})
+    and setting in ('language', 'platform', 'allowCustom', 'allowInline',
+      'createPrivateChannel', 'respond_to_settings', 'delete_after_respond', 'delete_response');`;
+
+  const settingResults = await db.query(channelQuery);
+
+  if (settingResults[0]) {
+    settingResults[0].map(row => ({
+      channel: row.channel_id,
+      setting: row.setting,
+      value: row.val,
+    })).forEach((row) => {
+      if (row.setting.indexOf('webhook') === -1) {
+        settings[`${row.channel}`][row.setting] = row.value;
+      }
+    });
+
+    /* eslint-disable no-param-reassign */
+    Object.keys(settings).forEach((channel) => {
+      const channelSettings = settings[channel];
+
+      if (!channelSettings.platform) {
+        channelSettings.platform = defaults.platform;
+      }
+
+      if (!channelSettings.prefix) {
+        channelSettings.prefix = defaults.prefix;
+      }
+      if (typeof channelSettings.allowCustom === 'undefined') {
+        channelSettings.allowCustom = defaults.allowCustom === '1';
+      } else {
+        channelSettings.allowCustom = channelSettings.allowCustom === '1';
+      }
+      if (typeof channelSettings.allowInline === 'undefined') {
+        channelSettings.allowInline = defaults.allowInline === '1';
+      } else {
+        channelSettings.allowInline = channel.allowInline === '1';
+      }
+
+      if (typeof channelSettings.defaultRoomsLocked === 'undefined') {
+        channelSettings.defaultRoomsLocked = defaults.defaultRoomsLocked === '1';
+      } else {
+        channelSettings.defaultRoomsLocked = channel.defaultRoomsLocked === '1';
+      }
+
+      if (typeof channelSettings.defaultNoText === 'undefined') {
+        channelSettings.defaultNoText = defaults.defaultNoText === '1';
+      } else {
+        channelSettings.defaultNoText = channel.defaultNoText === '1';
+      }
+
+      if (typeof channelSettings.defaultShown === 'undefined') {
+        channelSettings.defaultShown = defaults.defaultShown === '1';
+      } else {
+        channelSettings.defaultShown = channel.defaultShown === '1';
+      }
+
+      if (typeof channelSettings.createPrivateChannel === 'undefined') {
+        channelSettings.createPrivateChannel = defaults.createPrivateChannel === '1';
+      } else {
+        channelSettings.createPrivateChannel = channel.createPrivateChannel === '1';
+      }
+    });
+    /* eslint-enable no-param-reassign */
+  }
+  return settings;
+}
+
+/**
+ * Get settings for single guild
+ * @param  {Object} guild guild without settings
+ * @returns {Object}      guild with settings
+ */
+async function getGuild(guild) { /* eslint-disable no-param-reassign */
+  guild.channels = await aggregateChannels(guild);
+
+  /*
+  'defaultRoomsLocked', 'defaultNoText', 'defaultShown',
+  'tempCategory', 'lfgChannel',
+   */
+  return guild;
+}
+/* End utility functions */
+
 
 router.get('/', async (req, res) => {
   logger.info(`Received ${req.method} request for ${req.originalUrl} from ${req.connection.remoteAddress}`);
@@ -29,39 +172,29 @@ router.get('/', async (req, res) => {
     title: 'Index',
     loggedIn: req.isAuthenticated(),
     user: req.user,
-    guilds: await GetGuilds(req)
+    guilds: await getGuilds(req),
   });
 });
 
 router.get('/guild/:id', async (req, res) => {
-  // Main guild page for dashboard
-  // db get guild?
-  res.render('guild', {
-    title: req.user.guilds[req.params.id].name,
-    loggedIn: req.isAuthenticated(),
-    user: req.user,
-    guilds: await GetGuilds(req)
-  })
+  if (req.isAuthenticated()) {
+    // Main guild page for dashboard
+    // db get guild?
+    let guild;
+    const filteredGuilds = req.user.guilds.filter(userGuild => userGuild.id === req.params.id);
+    if (filteredGuilds.length) {
+      ([guild] = filteredGuilds);
+    }
+    res.render('guild', {
+      title: guild ? guild.name : 'Nobody\'s Here',
+      loggedIn: req.isAuthenticated(),
+      user: req.user,
+      guilds: await getGuilds(req),
+      guild: guild ? await getGuild(guild) : {},
+    });
+  } else {
+    res.redirect('/');
+  }
 });
 
-// doing this as we need to get the guild list each time they go to a page. or if you want to do it another way we can
-async function GetGuilds(req) {
-  let guilds;
-  if (req.isAuthenticated()) {
-    guilds = req.user.guilds;
-    const guildIds = guilds.map(guild => guild.id);
-
-    const query = SQL `select distinct guild_id from channels where guild_id in (${guildIds});`;
-    const queryResult = (await db.query(query))[0].map(row => row.guild_id);
-
-    guilds = guilds.filter(guild => {
-      const hasGuild = queryResult.indexOf(guild.id) !== -1;
-
-      const perm = new Discord.Permissions(guild.permissions);
-      const hasPerm = perm.has('MANAGE_ROLES', true);
-      return hasPerm && hasGuild;
-    });
-  }
-  return guilds
-}
 module.exports = router;
